6a4f6998798c4072d368d3a5340eec748bb12a38
DSB88 <diego.brandao@outlook.com>
2021-04-21 17:32:17 -0300

    Add Ex 03

--- Linguagem C/main.c
+++ Linguagem C/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#define ex2
+#define ex3
 
 #ifdef ex1
 
@@ -112,5 +112,56 @@ main()
 
 
 #endif ex2
+
+#ifdef ex3
+
+/* Problema "idades"
+
+Fazer um programa para ler o nome e idade de duas pessoas. Ao final mostrar uma mensagem com os
+nomes e a idade média entre essas pessoas, com uma casa decimal, conforme exemplo.
+
+Exemplo:
+Dados da primeira pessoa:
+Nome: Maria Silva
+Idade: 19
+Dados da segunda pessoa:
+Nome: Joao Melo
+Idade 20
+A idade média de Maria Silva e Joao Melo é de 19.5 anos
+*/
+
+main()
+{
+
+    setlocale(LC_ALL, "Portuguese");
+
+
+    float media,idade,idade2;
+    char nome[50],nome2[50];
+
+
+    printf("\Dados da primeira pessoa:\n ");
+    printf("\nNome: ");
+    gets(nome);
+
+    printf("Idade: ");
+    scanf("%f",&idade);
+
+    printf("\nDados da segunda pessoa:\n ");
+    printf("\nNome: ");
+    fseek(stdin, 0, SEEK_END); //Função necessária para leitura do gets.
+    gets(nome2);
+    printf("Idade: ");
+    scanf("%f",&idade2);
+
+
+    media = (idade+idade2) / 2;
+
+    printf("\nA idade média de %s e %s é de %.1f anos",nome,nome2,media);
+
+
+}
+
+#endif ex3
 
 
