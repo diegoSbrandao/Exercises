cb6c9102145afa8b09f4a3d314a9938072f3d696
DSB88 <diego.brandao@outlook.com>
2021-04-27 00:39:40 -0300

    Add Ex

---
+++ Linguagem Java/Estrutura Condicional/Ex02.java
@@ -0,0 +1,51 @@
+/*
+ * Exercício 02
+Correção: https://github.com/acenelio/nivelamento-java/blob/master/src/cond02.java
+Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+Exemplos:
+
+Entrada: Saída:
+12 PAR
+
+Entrada: Saída:
+-27 IMPAR
+
+Entrada: Saída:
+0 PAR
+ */
+
+import java.util.Scanner;
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		n=0;
+		
+		System.out.println("Digite um número");
+		n=sc.nextInt();
+		
+			if(n>=0) {
+				System.out.println("PAR");				
+			}
+			else {
+				System.out.println("ÍMPAR");
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+	
+}
